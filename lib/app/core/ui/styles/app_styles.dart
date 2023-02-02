1ddac76790ca57f91377497e13e942aeb8f244f9
Paulo Weslley <vorfast@gmail.com>
2023-02-01 21:27:19 -0300

    configuração de styles

---
+++ lib/app/core/ui/styles/app_styles.dart
@@ -0,0 +1,26 @@
+import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
+import 'package:flutter/material.dart';
+
+import 'colors_app.dart';
+
+class AppStyles {
+  static AppStyles? _instance;
+  AppStyles._();
+
+  static AppStyles get instace {
+    _instance ??= AppStyles._();
+    return _instance!;
+  }
+
+  ButtonStyle get primaryButton => ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(7),
+        ),
+        backgroundColor: ColorsApp.instace.primary,
+        textStyle: TextStyles.instace.textButtonLabel,
+      );
+}
+
+extension AppStylesExtensions on BuildContext {
+  AppStyles get appStyles => AppStyles.instace;
+}
