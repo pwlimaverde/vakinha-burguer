a252ce21980345b774710230ffa0d90f6c5954fb
Paulo Weslley <vorfast@gmail.com>
2023-02-04 19:23:17 -0300

    config rest client

---
+++ lib/app/core/rest_client/custom_dio.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:dio/native_imp.dart';
+
+import '../config/env/env.dart';
+
+class CustomDio extends DioForNative {
+  CustomDio()
+      : super(
+          BaseOptions(
+            baseUrl: Env.instace["backend_base_url"] ?? "",
+            connectTimeout: 5000,
+            receiveTimeout: 60000,
+          ),
+        ) {
+    interceptors.add(
+      LogInterceptor(
+        requestBody: true,
+        responseBody: true,
+        requestHeader: true,
+      ),
+    );
+  }
+
+  CustomDio auth() {
+    return this;
+  }
+
+  CustomDio unauth() {
+    return this;
+  }
+}
