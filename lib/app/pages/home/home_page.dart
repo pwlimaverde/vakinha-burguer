7b61d0fdb986532b7eb4aae64bc1cc3db8dd205b
Paulo Weslley <vorfast@gmail.com>
2023-02-04 21:58:26 -0300

    config Home Module

---
+++ lib/app/pages/home/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ColoredBox(
+        color: const Color(0XFF140E0E),
+        child: Stack(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
